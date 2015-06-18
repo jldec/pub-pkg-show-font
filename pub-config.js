e51eeec7c54b00cecac932d087d237497a44c65c
Jurgen Leschner <jldec@ciaosoft.com>
2015-06-18 15:55:22 -0400

    first commit

---
+++ pub-config.js
@@ -0,0 +1,8 @@
+var opts = module.exports = {
+  docTitle:    'pub-pkg-show-font',
+  appUrl:      'http://jldec.github.io/pub-pkg-show-font',
+  github:      'http://github.com/jldec/pub-pkg-show-font',
+  sources:     './show-font.md',
+  injectJs:    '/js/show-font.js',
+  copyright:   'Copyright (c) 2015 Jürgen Leschner - github.com/jldec - MIT License'
+}
